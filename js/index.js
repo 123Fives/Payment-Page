a53f31483fd526a7bb099cd264e12a69de9437c9
Veronica Highsmith <veronicahighsmith@yahoo.com>
2017-04-16 16:39:18 -0700

    Added coffeescript and js files

---
+++ js/index.js
@@ -0,0 +1,10 @@
+(function() {
+  $('.card').on('click', function(e) {
+    e.preventDefault();
+    $('.card').removeClass('active');
+    $(this).addClass('active');
+    $('.form').stop().slideUp();
+    $('.form').delay(300).slideDown();
+  });
+
+}).call(this);
